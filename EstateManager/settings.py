bf8a08d04225d597797d231323d38664d22b5fd2
ferachi <mkvezeh@gmail.com>
2017-04-25 19:30:43 +0100

    changed  the STATIC_ROOT path

--- EstateManager/settings.py
+++ EstateManager/settings.py
@@ -147,12 +147,12 @@ DATABASES['default'].update(db_from_env)
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(PROJECT_ROOT, 'static'),
     os.path.join(BASE_DIR, "static", "js"),
     os.path.join(BASE_DIR, "static", "css"),
     os.path.join(BASE_DIR, "static", "img"),
@@ -161,4 +161,3 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static", "css", "vendors"),
 )
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
